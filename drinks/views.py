b57d2f25c74204f2fa7815d88973497a38d62ebb
Prakriti Pokharel <iprakritip@gmail.com>
2023-09-08 12:57:49 +0545

    feat : create a functionality to post a new drink

--- drinks/views.py
+++ drinks/views.py
@@ -1,9 +1,20 @@
 from django.http import JsonResponse
+from http import HTTPStatus
 from drinks.models import Drink
 from drinks.serializers import DrinkSerializer
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
+
+@api_view(['GET','POST'])
 def drink_list(request):
-
-    drinks=Drink.objects.all()
-    serializer=DrinkSerializer(drinks, many=True)
-    return JsonResponse({'drinks':serializer.data})
+    if request.method=='GET':
+        drinks=Drink.objects.all()
+        serializer=DrinkSerializer(drinks, many=True)
+        return JsonResponse({'drinks':serializer.data})
+    
+    if request.method=='POST':
+        serializer =  DrinkSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save();
+            return Response(serializer.data, status= HTTPStatus.CREATED)
