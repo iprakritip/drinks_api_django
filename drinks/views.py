faa3351a36c892e7893de821476a543a634de712
Prakriti Pokharel <iprakritip@gmail.com>
2023-09-08 13:04:51 +0545

    feat : access drinks by using their id on route

--- drinks/views.py
+++ drinks/views.py
@@ -18,3 +18,18 @@ def drink_list(request):
         if serializer.is_valid():
             serializer.save();
             return Response(serializer.data, status= HTTPStatus.CREATED)
+
+@api_view(['GET','PUT','DELETE'])
+def drink_detail(request,id):
+    try:
+        drink = Drink.objects.get(pk=id)
+    except Drink.DoesNotExist:
+        return Response(status=HTTPStatus.NOT_FOUND)
+
+    if request.method == 'GET':
+        serializer = DrinkSerializer(drink)
+        return Response(serializer.data, status=HTTPStatus.OK)
+    elif request.method == 'PUT':
+        pass
+    if request.method == 'DELETE':
+        pass
